2fe0d26e37d4b63a5e489c59f201e470c41414b3
Daniela Isabel Bolivar Garcia <daniela.bolivar@globant.com>
2020-10-27 16:23:47 -0500

    babel script

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _Footer = require("./Footer");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Footer = function Footer(props) {
+  /*#__PURE__*/
+  _react["default"].createElement(_Footer.StyledFooter, props);
+};
+
+var _default = Footer;
+exports["default"] = _default;
